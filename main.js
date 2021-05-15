35819e067954fc6bb565dbf091b23182c80da77d
magalipujol <magalipuuujol@gmail.com>
2021-05-15 15:05:27 -0300

    understanding vue

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+//in the documentation app it's called vm
 let app = new Vue({
-
-})
+    //DOM element
+  el: "#app",
+  //Data element
+  data: {
+    title: "My portfolio",
+    titleHTML: "John Dow <span class='badge'>portfolio</span>",  
+    alert: "This is an alert message!",
+    projects: [
+      { title: "portfolio", desc: "Lorem ipsum" },
+      { title: "twitter clone", desc: "Lorem ipsum" },
+    ],
+  },
+});
