195da422cbfdad098067173e4ed075385835efda
magalipujol <magalipuuujol@gmail.com>
2021-05-16 18:08:32 -0300

    registration form created

--- main.js
+++ main.js
@@ -5,23 +5,42 @@ let app = new Vue({
   //Data element
   data: {
       userData: {},
+      // this variable tries to make de app more realistic, 
+      // but I don't have a database to store all user details
+      usersID: 0,
       name: "",
       email: "",
       password: "",
       max_length: 25,
       max_pass_length:16,    
+      error: "",
+      // this is used to display or hide the registration form
+      registered: false,
     },
   methods: {
-    registerAccount() {
-      // this variables record user info
-      this.userData.name = ""
-      this.userData.email = ""
-      this.userData.password = ""
-
-      // this variables are used when you want to clear registration fields
-      this.name = ""
-      this.email = ""
-      this.password = ""
+    registerAccount(){
+      if (this.name !== "" && this.email !== "" && this.password !== "" ) 
+      {
+          this.userData.id = ++this.usersID,
+          this.userData.name = this.name,
+          this.userData.email = this.email,
+          this.userData.password = this.password
+           
+      }  else {
+        this.error = "Complete all the form fields"
     }
+  
+  // Add registration data to the local storage 
+localStorage.setItem('simple_tweet_registered', true)
+// add the whole userData object as JSON string
+localStorage.setItem('simple_tweet_registered_user', JSON.stringify(this.userData))
+  
+  // Clear the registration inputs 
+  this.name = "";
+  this.email = "";
+  this.password = "";
+  }
+  
+}
   },
-})
+)
